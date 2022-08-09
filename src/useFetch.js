e79a6e87d4a17c253f1f01ad37f586e70643f8f0
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-08-09 11:32:51 +0300

    create simple blog using React, json-server

---
+++ src/useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+ 
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    fetch(url, {signal: abortCont.signal})
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data from this resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch((e) => {
+        if(e.name === "AbortError") {
+            console.log("fetch aborted")
+        } else {
+            setIsPending(false);
+            setError(e.message);
+        }
+      });
+      return () => abortCont.abort();
+  }, [url]);
+  return {data, isPending, error}
+};
